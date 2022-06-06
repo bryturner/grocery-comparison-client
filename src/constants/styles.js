a0fa37bfa2a9c9d5a5a5a2ca7c56882c4f62a519
bryan.turner.ch@gmail.com <bryan.turner.ch@gmail.com>
2022-06-06 18:32:12 +0200

    update to styles

--- src/constants/styles.js
+++ src/constants/styles.js
@@ -1,3 +1,12 @@
 export const BORDER = {
   MAIN: ["border: 2px solid black;", "border-radius: 8px;", "padding: 0.5rem;"],
 };
+
+export const LIST = {
+  PRODUCT: [
+    "list-style: none;",
+    "display: flex;",
+    "flex-direction: column;",
+    "gap: 0.5rem;",
+  ],
+};
